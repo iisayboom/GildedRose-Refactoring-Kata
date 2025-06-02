ed260e623709652293279c72820ae51758189456
Niels De Bruyne <niels.debruyne@dxc.com>
2025-06-02 16:20:19 +0200

    Imrpove readability of BackstagePassBehavior

--- Java/src/main/java/com/gildedrose/behavior/BackstagePassBehavior.java
+++ Java/src/main/java/com/gildedrose/behavior/BackstagePassBehavior.java
@@ -3,18 +3,30 @@ package com.gildedrose.behavior;
 import com.gildedrose.Item;
 
 public class BackstagePassBehavior implements ItemBehavior {
+
+    private static final int MAX_QUALITY = 50;
+
     @Override
     public void update(Item item) {
         item.sellIn--;
 
         if (item.sellIn < 0) {
             item.quality = 0;
-        } else if (item.sellIn < 5) {
-            item.quality = Math.min(50, item.quality + 3);
-        } else if (item.sellIn < 10) {
-            item.quality = Math.min(50, item.quality + 2);
-        } else {
-            item.quality = Math.min(50, item.quality + 1);
+            return;
         }
+
+        item.quality += getQualityIncrease(item.sellIn);
+        if (item.quality > MAX_QUALITY) {
+            item.quality = MAX_QUALITY;
+        }
+    }
+
+    private int getQualityIncrease(int sellIn) {
+        if (sellIn < 5)
+            return 3;
+        if (sellIn < 10)
+            return 2;
+
+        return 1;
     }
 }
